d89a48d867a2a92a99c4a5c52402b19e53b2e073
yasukitachibana <yasuki.tachibana@gmail.com>
2022-02-08 23:06:29 +0900

    latest

--- src/RunPythia.cc
+++ src/RunPythia.cc
@@ -72,88 +72,86 @@ void RunPythia::SetHadronizationParameters()
 
 void RunPythia::ExecOnePtHatBin(int i)
 {
-  // Pythia8::Pythia pythia;
+  Pythia8::Pythia pythia;
+  //----------------------------------------------------
+  // Set PtHat Min and Max
+  std::string string_pthatmin = "PhaseSpace:pTHatMin = " + pTHatMin[i];
+  std::string string_pthatmax = "PhaseSpace:pTHatMax = " + pTHatMax[i];
+  pythia.readString(string_pthatmin);
+  pythia.readString(string_pthatmax);
+  //----------------------------------------------------
 
-  std::unique_ptr<Pythia8::Pythia> pythia;
-  // //----------------------------------------------------
-  // // Set PtHat Min and Max
-  // std::string string_pthatmin = "PhaseSpace:pTHatMin = " + pTHatMin[i];
-  // std::string string_pthatmax = "PhaseSpace:pTHatMax = " + pTHatMax[i];
-  // pythia.readString(string_pthatmin);
-  // pythia.readString(string_pthatmax);
-  // //----------------------------------------------------
+  //----------------------------------------------------
+  // Set Other Parameters
+  for (auto param : parameter_strings)
+  {
+    pythia.readString(param);
+  }
+  //----------------------------------------------------
 
-  // //----------------------------------------------------
-  // // Set Other Parameters
-  // for (auto param : parameter_strings)
-  // {
-  //   pythia.readString(param);
-  // }
-  // //----------------------------------------------------
-
-  // pythia.init();
-  // std::string particle_list_filename = SetFile::Instance()->GetParticleListFileName(pTHatMin[i], pTHatMax[i], hadronization);
-  // std::string sigma_filename = SetFile::Instance()->GetSigmaFileName(pTHatMin[i], pTHatMax[i]);
-  // //-
-  // std::ofstream f_particle, f_sigma;
-  // f_particle.open(particle_list_filename, std::ios::out);
-  // f_sigma.open(sigma_filename, std::ios::out);
-  // //-
-  // std::cout << "======================================================================================================" << std::endl;
-  // std::cout << "[RunPythia] Start RunPythia" << std::endl;
-  // std::cout << "pTHat: " << pTHatMin[i] << "-" << pTHatMax[i] << " GeV/c" << std::endl;
-  // std::cout << "output particle list file: " << particle_list_filename << std::endl;
-  // std::cout << "output sigma_hard file: " << sigma_filename << std::endl;
-  // std::cout << "======================================================================================================" << std::endl;
-  // //-
-  // int p_stat = 0;
-  // int i_ev = 0;
-  // for (int i_run = 0; i_run < nEvents; ++i_run)
-  // {
-  //   //------------------
-  //   if (!pythia.next())
-  //   {
-  //     continue;
-  //   }
-  //   //------------------
-  //   if ((20 * i_ev) % nEvents == 0)
-  //   {
-  //     std::cout << "Event# " << i_ev << "...";
-  //   }
-  //   //------------------
-  //   f_particle << "#\tEvent" << i_ev + 1
-  //              << "ID\tpstat\tE\tPx\tPy\tPz\tEta\tPhi" << std::endl;
-  //   //------------------
-  //   int sn = 0;
-  //   for (int i = 0; i < pythia.event.size(); ++i)
-  //   {
-  //     if (pythia.event[i].isFinal() && fabs(pythia.event[i].eta()) < 20)
-  //     {
-  //       f_particle << sn << "\t"
-  //                  << pythia.event[i].id() << "\t"
-  //                  << p_stat << "\t"
-  //                  << pythia.event[i].e() << "\t"
-  //                  << pythia.event[i].px() << "\t"
-  //                  << pythia.event[i].py() << "\t"
-  //                  << pythia.event[i].pz() << "\t"
-  //                  << pythia.event[i].eta() << "\t"
-  //                  << pythia.event[i].phi() << std::endl;
-  //       sn++;
-  //     }
-  //   }
-  //   //------------------
-  //   i_ev++;
-  // }
-  // std::cout << "\nDONE! Total Event Number: " << i_ev << std::endl;
-  // //------------------
-  // f_sigma << pythia.info.sigmaGen() << "\t"
-  //         << pythia.info.sigmaErr() << std::endl;
-  // std::cout << "sigma_hard = "
-  //           << pythia.info.sigmaGen() << "+/-"
-  //           << pythia.info.sigmaErr() << std::endl;
-  // //------------------
-  // f_particle.close();
-  // f_sigma.close();
+  pythia.init();
+  std::string particle_list_filename = SetFile::Instance()->GetParticleListFileName(pTHatMin[i], pTHatMax[i], hadronization);
+  std::string sigma_filename = SetFile::Instance()->GetSigmaFileName(pTHatMin[i], pTHatMax[i]);
+  //-
+  std::ofstream f_particle, f_sigma;
+  f_particle.open(particle_list_filename, std::ios::out);
+  f_sigma.open(sigma_filename, std::ios::out);
+  //-
+  std::cout << "======================================================================================================" << std::endl;
+  std::cout << "[RunPythia] Start RunPythia" << std::endl;
+  std::cout << "pTHat: " << pTHatMin[i] << "-" << pTHatMax[i] << " GeV/c" << std::endl;
+  std::cout << "output particle list file: " << particle_list_filename << std::endl;
+  std::cout << "output sigma_hard file: " << sigma_filename << std::endl;
+  std::cout << "======================================================================================================" << std::endl;
+  //-
+  int p_stat = 0;
+  int i_ev = 0;
+  for (int i_run = 0; i_run < nEvents; ++i_run)
+  {
+    //------------------
+    if (!pythia.next())
+    {
+      continue;
+    }
+    //------------------
+    if ((20 * i_ev) % nEvents == 0)
+    {
+      std::cout << "Event# " << i_ev << "...";
+    }
+    //------------------
+    f_particle << "#\tEvent" << i_ev + 1
+               << "ID\tpstat\tE\tPx\tPy\tPz\tEta\tPhi" << std::endl;
+    //------------------
+    int sn = 0;
+    for (int i = 0; i < pythia.event.size(); ++i)
+    {
+      if (pythia.event[i].isFinal() && fabs(pythia.event[i].eta()) < 20)
+      {
+        f_particle << sn << "\t"
+                   << pythia.event[i].id() << "\t"
+                   << p_stat << "\t"
+                   << pythia.event[i].e() << "\t"
+                   << pythia.event[i].px() << "\t"
+                   << pythia.event[i].py() << "\t"
+                   << pythia.event[i].pz() << "\t"
+                   << pythia.event[i].eta() << "\t"
+                   << pythia.event[i].phi() << std::endl;
+        sn++;
+      }
+    }
+    //------------------
+    i_ev++;
+  }
+  std::cout << "\nDONE! Total Event Number: " << i_ev << std::endl;
+  //------------------
+  f_sigma << pythia.info.sigmaGen() << "\t"
+          << pythia.info.sigmaErr() << std::endl;
+  std::cout << "sigma_hard = "
+            << pythia.info.sigmaGen() << "+/-"
+            << pythia.info.sigmaErr() << std::endl;
+  //------------------
+  f_particle.close();
+  f_sigma.close();
 }
 
 void RunPythia::Exec()
